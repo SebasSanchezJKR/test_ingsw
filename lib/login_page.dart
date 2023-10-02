1e77a18270f7290c5ffea5e4ea8d5ad816507fa6
Sebastián Sánchez Granados <summernutria12@gmail.com>
2023-10-02 13:39:03 -0500

    feat: login page bgcolor

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,7 +6,28 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Image.asset("JudeGOD.jpg"),
+      backgroundColor: const Color(0xFF025951),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(),
+          const Text(
+            "Sistema para la prevención de problemas cardíacos",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 48,
+            ),
+          ),
+          const Spacer(),
+          TextFormField(),
+          const Spacer(),
+          const SizedBox(
+            height: 60,
+            width: 120,
+            child: InkWell(),
+          ),
+        ]
+      ),
     );
   }
 }
